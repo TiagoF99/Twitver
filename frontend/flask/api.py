6a3997d6daf0c908392e204ac3310289da1e0e83
t.ferreria@live.ca <t.ferreira@live.ca>
2020-04-17 00:50:22 -0400

    allow one client to connect and send/recieve messages

--- frontend/flask/api.py
+++ frontend/flask/api.py
@@ -4,74 +4,81 @@ import sys
 from ctypes import *
 
 app = Flask(__name__)
+s_sock = None
 
-@app.route('/test', methods=['GET', 'POST'])
-def add():
-
-    # POST request
-    if request.method == 'POST':
+"""
+POST REQUEST:
+    if request.method == 'post':
         nums = request.get_json()
-        # must return a string value and note nums is a dict of strings
-        print(nums)
         return "hello"
 
-    # GET request
-    else:
-        message = {"message": main()}
-        print(message)
-        return jsonify(message)  # serialize and use JSON headers
+s.sendall(bytes(input(server_output), 'utf-8'))
+"""
+
+@app.route('/connect', methods=['GET', 'POST'])
+def connect():
+    global s_sock
+
+    # POST request
+    if request.method == 'GET':
+        server_addr = ('localhost', 55248)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            s.connect(server_addr)
+            print("Connected to %s" % repr(server_addr))
+            if not s_sock:
+                s_sock = s
+        except:
+            print("ERROR: Connection to %s refused" % repr(server_addr))
+            sys.exit(1)
+
+        try:
+            buff = s.recv(256)
+            server_output = str(buff).replace('\'b\'', '').strip('b').replace(
+                "\'", "").replace("\\r\\n", "").strip()
+            print({"message": server_output})
+            return jsonify({"message": server_output})  # serialize and use JSON headers
+        except:
+            print("Closing socket")
+            s.close()
 
 
-""" This class defines a C-like struct """
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    global s_sock
+    # POST request
+    if request.method == 'POST':
+        if s_sock:
+            data = request.get_json()
+            print("data recieved: {}".format(data))
+            try:
+                # send your input
+                print(data["input"])
+                s_sock.sendall(bytes(data["input"] + "\r\n", 'utf-8'))
+
+                print("here")
+                # receive back a response
+                buff = s_sock.recv(256)
+                print("now here")
+                server_output = str(buff).replace('\'b\'', '').strip('b').replace("\'", "").replace("\\r\\n", "").strip()
+                print({"message": server_output})
+                return jsonify({"message": server_output})
+            except:
+                print("Closing socket in write()")
+                s_sock.close()
+        else:
+            print("s_sock is NULL")
+
+
 class Payload(Structure):
+    """
+    This class defines a C-like struct
+    """
     _fields_ = [("id", c_uint32),
                 ("counter", c_uint32),
                 ("temp", c_float)]
 
-
-def main():
-    server_addr = ('localhost', 55248)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # if s < 0:
-    #     print("Error creating socket")
-
-    try:
-        s.connect(server_addr)
-        print("Connected to %s" % repr(server_addr))
-    except:
-        print("ERROR: Connection to %s refused" % repr(server_addr))
-        sys.exit(1)
-
-    try:
-        # while True:
-            # print("")
-            # payload_out = Payload(1, i, random.uniform(-10, 30))
-            # print("Sending id=%d, counter=%d, temp=%f" % (payload_out.id,
-            #                                             payload_out.counter,
-            #                                             payload_out.temp))
-            # nsent = s.send(payload_out)
-            # Alternative: s.sendall(...): coontinues to send data until either
-            # all data has been sent or an error occurs. No return value.
-            # print("Sent %d bytes" % nsent)
-
-
-        buff = s.recv(256)
-        # payload_in = Payload.from_buffer_copy(buff)
-
-        server_output = str(buff).replace('\'b\'', '').strip('b').replace("\'", "")
-        # print("Received id=%d, counter=%d, temp=%f" % (payload_in.id,
-        #                                             payload_in.counter,
-        #                                             payload_in.temp))
-        return server_output
-
-        #s.sendall(bytes(input(server_output), 'utf-8'))
-        # Alternative: s.sendall(...): coontinues to send data until either
-        # all data has been sent or an error occurs. No return value.
-        # print("Sent %d bytes" % nsent)
-
-    finally:
-        print("Closing socket")
-        s.close()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 
+   
     constructor(props) {
         super(props);
-        this.state = {message: "before"};
+        this.state = {messages: [], input: "", count: 0};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
       }
 
 
     componentDidMount() {
         console.log("starting...")
-        fetch('/test', {
+        fetch('/connect', {
             method: 'get',
             headers: {
               'Accept': 'application/json, text/plain, */*',
@@ -22,27 +26,84 @@ class App extends Component {
             }).then(text => {
                 console.log(text);
                 console.log(text["message"]);
-                this.setState({message: text["message"]});
+                this.add_message("server message:", text["message"]);
+                console.log(this.state.messages);
             });
     }
+
+    add_message = (title, message) => {
+      let arr = [];
+      arr.push(title);
+      arr.push(message);
+      this.state.messages.push(arr)
+      let new_count = this.state.count + 1;
+      this.setState({count: new_count});
+    } 
+
+    handleChange(event) {
+        this.setState({input: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.input)
+        fetch('/write', {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({input: this.state.input})
+            }).then(response => {
+                return response.json();
+            }).then(text => {
+                console.log(text);
+                console.log(text["message"]);
+                this.add_message("User Input:", this.state.input);
+                this.add_message("server message:", text["message"]);
+                console.log(this.state.messages);
+            });
+        event.preventDefault();
+    }
+
+
 
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p> message: {this.state.message} </p>
+          <div className="container-fluif">
+            <div className="row">
+              <div className="col-6">
+                <img id="twitter_image" src="/assets/twitter_icon.png" className="card-img-top" alt="..." />
+              </div>
+              <div className="col-6">
+                <h2 id="title"> Welcome to Twitver! </h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="list-group">
+            <ul>
+              {this.state.messages.map((value, index) => {
+                return (
+                  <a href="#" className="list-group-item list-group-item-action">
+                    <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">{value[0]}</h5>
+                    </div>
+                    <p className="mb-1">{value[1]}</p>
+                  </a>
+                )})}
+            </ul>
+          </div>
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Your Input:</label>
+              <input value={this.state.input} onChange={this.handleChange} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </header>
       </div>
     );
